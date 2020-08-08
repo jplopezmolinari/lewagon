7780e8592f81ea89806b7c1ada6d79bc512c6b59
JP Lopez Molinari <jplopezmolinari@gmail.com>
2020-08-08 19:05:33 -0300

    Add files via upload

--- JS/validation.js
+++ JS/validation.js
@@ -1,1 +1,38 @@
 const form = document.querySelector("#info")
+const email = document.querySelector("#email");
+const emailHelp = document.querySelector("#emailHelp");
+const password = document.querySelector("#password");
+const passwordHelp = document.querySelector("#passwordHelp");
+
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault()
+
+if (email.value === "" && password.value === ""){
+	
+		emailHelp.innerText = "Por favor coloque su email"
+		passwordHelp.innerText = "Por favor coloque su contraseña"
+	
+} else {
+	
+	if (email.value != "" && password.value === ""  ) {
+		emailHelp.innerText= "       "
+		passwordHelp.innerText = "Por favor coloque su contraseña"
+	}
+
+
+	if (email.value === "" && password.value != "") {
+		emailHelp.innerText = "Por favor coloque su email"
+		passwordHelp.innerText = "   "
+	} else {
+		if (email.value != "" && password.value != "") {
+			form.submit();
+			console.log("Funciona")
+		}
+		
+	}
+	
+}
+
+	console.log("Intentó iniciar sesión")
+})
